43a2fadb6c6b969f97bb07c7146993bd70bdf092
mmeyer2k <1887431+mmeyer2k@users.noreply.github.com>
2022-03-10 16:58:34 -0600

    Create SqliProtection.php

---
+++ src/SqliProtection.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Mmeyer2k\LaravelSqliGuard;
+
+class SqliProtection extends SP
+{
+    private const configString = 'sqliguard.allow_unsafe_mysql';
+
+    public static function isUnsafeAllowed(): ?bool
+    {
+        return config(self::configString);
+    }
+
+    public static function allowUnsafe(): void
+    {
+        config([self::configString => true]);
+    }
+
+    public static function blockUnsafe(): void
+    {
+        config([self::configString => false]);
+    }
+}
